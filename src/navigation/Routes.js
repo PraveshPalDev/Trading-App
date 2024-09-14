15e5739d688dd5da2c3d2baf087ab4c263d4be66
zenesys0081 <pravesh.kumar2@zenesys.com>
2024-09-14 12:23:41 +0530

    setup the drawer navigation

--- src/navigation/Routes.js
+++ src/navigation/Routes.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import MainStack from './MainStack';
 import AuthStack from './AuthStack';
 import {useSelector} from 'react-redux';
+import DrawerStack from './DrawerStack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function Routes() {
   const userData = useSelector(state => state.auth.userData);
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        {!!userData?.token ? MainStack(Stack) : AuthStack(Stack)}
-      </Stack.Navigator>
+      {true ? (
+        DrawerStack(Drawer)
+      ) : (
+        <Stack.Navigator>{AuthStack(Stack)}</Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
