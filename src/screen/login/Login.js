12e00b6655c07dd544ec87f95cc0ac1145fae18a
zenesys0081 <pravesh.kumar2@zenesys.com>
2024-09-14 10:45:30 +0530

    added the react-native-vector-icon in android and ios both

--- src/screen/login/Login.js
+++ src/screen/login/Login.js
@@ -1,11 +1,14 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import WrapperContainer from '../../components/WrapperContainer';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Login() {
   return (
-    <View>
+    <WrapperContainer>
       <Text>Login</Text>
-    </View>
+      <Icon name="rocket" size={30} color="#900" />
+    </WrapperContainer>
   );
 }
 
